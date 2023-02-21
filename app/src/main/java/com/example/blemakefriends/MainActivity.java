934a559b5f03d36cc6e54692b2c15becad0f8609
Harry Lin <harry.lin@hundredstep.com>
2023-02-22 07:54:24 +0800

    [UPDATE] Add onActivityResult for access(Online App Changed)

--- app/src/main/java/com/example/blemakefriends/MainActivity.java
+++ app/src/main/java/com/example/blemakefriends/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.blemakefriends;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,21 +8,26 @@ import android.util.Log;
 import android.widget.Button;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.CallbackManager;
 import com.facebook.login.Login;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 
+import org.json.JSONObject;
+
 import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "HrudredStep";
+    private static final String TAG = "HundredStep";
     private Button facebookButton;
     CallbackManager callbackManager;
     @Override
@@ -31,11 +37,15 @@ public class MainActivity extends AppCompatActivity {
         facebookButton = findViewById(R.id.FaceBookButton);
         facebookButton.setOnClickListener(view -> facebookClick());
         callbackManager = CallbackManager.Factory.create();
+        Log.d(TAG,"APP Start");
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
                 Log.d(TAG,"==========Login Success==========");
+                Log.d(TAG,loginResult.toString());
+                Log.d(TAG,"USER ID=" + loginResult.getAccessToken().getUserId());
+
             }
 
             @Override
@@ -55,6 +65,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+        GraphRequest graph = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(@Nullable JSONObject jsonObject, @Nullable GraphResponse graphResponse) {
+                Log.d(TAG,jsonObject.toString());
+            }
+        });
     }
     public void facebookClick(){
         Log.d(TAG,"This is button!!!");
