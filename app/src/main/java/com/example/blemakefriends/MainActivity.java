3cfbf750aa2f284a6475699e384d0231f7072229
Harry Lin <harry.lin@hundredstep.com>
2023-02-21 15:59:08 +0800

    [UPDATE] facebook login success
    1. After modify build.gradle file , need to click sync to download facebook sdk
    2. keytool -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore | openssl sha1 -binary | openssl base64

--- app/src/main/java/com/example/blemakefriends/MainActivity.java
+++ app/src/main/java/com/example/blemakefriends/MainActivity.java
@@ -2,23 +2,62 @@ package com.example.blemakefriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.util.Log;
 import android.widget.Button;
 import android.os.Bundle;
+
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager;
+import com.facebook.login.Login;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "HrudredStep";
     private Button facebookButton;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         facebookButton = findViewById(R.id.FaceBookButton);
         facebookButton.setOnClickListener(view -> facebookClick());
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d(TAG,"==========Login Success==========");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d(TAG,"==========Login ERROR==========");
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
     public void facebookClick(){
         Log.d(TAG,"This is button!!!");
+        LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile"));
     }
 }
