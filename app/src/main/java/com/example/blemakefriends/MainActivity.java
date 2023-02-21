323d5124a4c918ce502faf5366fd68780c717770
Harry Lin <harry.lin@hundredstep.com>
2023-02-21 12:26:01 +0800

    [UPDATE] Add First button and onClickLister

--- app/src/main/java/com/example/blemakefriends/MainActivity.java
+++ app/src/main/java/com/example/blemakefriends/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.blemakefriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "HrudredStep";
+    private Button facebookButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        facebookButton = findViewById(R.id.FaceBookButton);
+        facebookButton.setOnClickListener(view -> facebookClick());
+    }
+
+    public void facebookClick(){
+        Log.d(TAG,"This is button!!!");
     }
 }
